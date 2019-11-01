b1007ea890001f13cbb954bf7ef7f0325ee2265d
Adrian Carpenter <adrian@fizzyade.com>
2019-11-01 16:57:41 +0000

    wip: more initial work

--- Sources/X1Kit/X1Kit.swift
+++ Sources/X1Kit/X1Kit.swift
@@ -25,15 +25,23 @@
 
 import CoreBluetooth
 
-protocol X1KitMouseDelegate {
-    func connectedStateDidChange(isConnected: Bool)
-    func mouseDidMove(x: Int, y: Int)
-    func buttonsDidChange(state: Int)
-    func wheelDidScroll(z: Int)
+enum X1MouseButton : UInt8 {
+    case Left = 0
+    case Right = 1
+    case Middle = 2
+}
+
+protocol X1KitMouseDelegate : class {
+    func connectedStateDidChange(identifier:UUID, isConnected: Bool)
+    func mouseDidMove(identifier:UUID, x: Int8, y: Int8)
+    func mouseDown(identifier:UUID, button: X1MouseButton)
+    func mouseUp(identifier:UUID, button: X1MouseButton)
+    func wheelDidScroll(identifier:UUID, z: Int8)
 }
 
 class X1Mouse: NSObject {
     var centralManager: CBCentralManager!
+    var x1Array: [CBPeripheral] = []
     weak var delegate: X1KitMouseDelegate?
     
     static let X1Service = CBUUID(string: "2B080000-BDB5-F6EB-24AE-9D6AB282AB63")
@@ -45,6 +53,22 @@ class X1Mouse: NSObject {
     
     func start() {
         centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+}
+
+extension CBCharacteristic {
+    private struct X1KitState {
+        static var buttonsState: UInt8 = 0
+    }
+    
+    var x1LastButtonsState:UInt8 {
+        get {
+            return X1KitState.buttonsState;
+        }
+        
+        set(newValue) {
+            X1KitState.buttonsState = newValue;
+        }
     }
 }
 
@@ -64,15 +88,16 @@ extension X1Mouse: CBCentralManagerDelegate {
                 break
                 
             case .poweredOff:
+                x1Array.removeAll()
                 break
                 
             case .poweredOn:
-                let peripheralList = centralManager.retrieveConnectedPeripherals(withServices: [X1Service])
+                x1Array = centralManager.retrieveConnectedPeripherals(withServices: [X1Mouse.X1Service])
                 
-                for peripheral in peripheralLost {
-                    peripheral.delegate = self
+                for x1 in x1Array {
+                    x1.delegate = self
 
-                    centralManager.connect(peripheral)
+                    centralManager.connect(x1)
                 }
                 
             default:
@@ -80,21 +105,23 @@ extension X1Mouse: CBCentralManagerDelegate {
         }
     }
     
-    func centralManager(_ central: CBCentralManager, 
+    func centralManager(_ central: CBCentralManager,
                   didConnect peripheral: CBPeripheral) {
-        delegate?.connectedStateDidChange(isConnected: true)
+        delegate?.connectedStateDidChange(identifier: peripheral.identifier, isConnected: true)
+        
+        peripheral.discoverServices(nil)
     }
     
-    func centralManager(_ central: CBCentralManager, 
-     didDisconnectPeripheral peripheral: CBPeripheral, 
+    func centralManager(_ central: CBCentralManager,
+     didDisconnectPeripheral peripheral: CBPeripheral,
                        error: Error?) {
-        delegate?.connectedStateDidChange(isConnected: false)
+        delegate?.connectedStateDidChange(identifier: peripheral.identifier, isConnected: false)
         
         centralManager.connect(peripheral)
     }
     
-    func centralManager(_ central: CBCentralManager, 
-            didFailToConnect peripheral: CBPeripheral, 
+    func centralManager(_ central: CBCentralManager,
+            didFailToConnect peripheral: CBPeripheral,
                        error: Error?) {
         centralManager.connect(peripheral);
     }
@@ -105,7 +132,7 @@ extension X1Mouse: CBPeripheralDelegate {
         guard let services = peripheral.services else { return }
         
         for service in services {
-            if (service.uuid==X1Service) {
+            if (service.uuid==X1Mouse.X1Service) {
                 peripheral.discoverCharacteristics(nil, for: service)
             }
         }
@@ -117,11 +144,11 @@ extension X1Mouse: CBPeripheralDelegate {
         
         for characteristic in characteristics {
             switch(characteristic.uuid) {
-                case characteristicProtocolMode:
+                case X1Mouse.characteristicProtocolMode:
                     /* select boot mode protocol */
                     peripheral.writeValue(Data(bytes:[0], count:1), for: characteristic, type: CBCharacteristicWriteType.withoutResponse)
 
-                case characteristicReport:
+                case X1Mouse.characteristicReport:
                     peripheral.discoverDescriptors(for: characteristic);
                 
                 default:
@@ -139,18 +166,18 @@ extension X1Mouse: CBPeripheralDelegate {
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor descriptor: CBDescriptor, error: Error?) {
-        if (descriptor.uuid==descriptorReportReference) {
+        if (descriptor.uuid==X1Mouse.descriptorReportReference) {
             if let value = descriptor.value as? NSData {
                 let reportId = (UInt16(value[0])<<8) | (UInt16(value[1]))
                 
                 switch(reportId) {
-                    case xyReport, wheelAndButtonsReport:
+                    case X1Mouse.xyReport, X1Mouse.wheelAndButtonsReport:
                         peripheral.setNotifyValue(true, for: descriptor.characteristic)
                         
                         if (descriptor.characteristic.properties.contains(.notify)) {
                             peripheral.setNotifyValue(true, for: descriptor.characteristic)
                         }
-
+                        
                         break
                     
                     default:
@@ -162,20 +189,43 @@ extension X1Mouse: CBPeripheralDelegate {
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic,
     error: Error?) {
-        if (characteristic.uuid==characteristicReport) {
+        if (characteristic.uuid==X1Mouse.characteristicReport) {
             for descriptor in characteristic.descriptors!{
-                if (descriptor?.uuid==descriptorReportReference) {
+                if (descriptor.uuid==X1Mouse.descriptorReportReference) {
                     if let value = descriptor.value as? NSData {
                         let reportId = (UInt16(value[0])<<8) | (UInt16(value[1]))
                         
                         switch(reportId) {
-                            case xyReport:
-                                delegate?.mouseDidMove(x: 0, y: 0)
+                            case X1Mouse.xyReport:
+                                if let xyValue = characteristic.value as NSData? {
+                                    delegate?.mouseDidMove(identifier: peripheral.identifier, x: Int8.init(bitPattern:xyValue[0]), y: Int8.init(bitPattern:xyValue[1]))
+                                }
                                 break
                                 
-                            case wheelAndButtonsReport:
-                                delegate?.buttonsDidChange(state: 0)
-                                delegate?.wheelDidScroll(z: 0)
+                            case X1Mouse.wheelAndButtonsReport:
+                                if let buttonsValue = characteristic.value as NSData? {
+                                    if (characteristic.x1LastButtonsState != UInt8.init(buttonsValue[0])) {
+                                        for bit:UInt8 in 0...2 {
+                                            if ( ((buttonsValue[0]) & (1<<bit)) != ((characteristic.x1LastButtonsState & (1<<bit))) ) {
+                                                if (((buttonsValue[0]) & (1<<bit))==(1<<bit)) {
+                                                    delegate?.mouseDown(identifier: peripheral.identifier, button: X1MouseButton(rawValue: bit)!)
+                                                }
+                                                else {
+                                                    delegate?.mouseUp(identifier: peripheral.identifier, button: X1MouseButton(rawValue: bit)!)
+                                                }
+                                            }
+                                        }
+                                        
+                                        characteristic.x1LastButtonsState = UInt8.init(buttonsValue[0])
+                                    }
+                                        
+                                    if (Int8.init(bitPattern: buttonsValue[1]) != 0) {
+                                        delegate?.wheelDidScroll(identifier: peripheral.identifier, z: Int8.init(bitPattern: buttonsValue[1]))
+                                    }
+                                }
+                                break
+                            
+                            default:
                                 break
                         }
                     }
